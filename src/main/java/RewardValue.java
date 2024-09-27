a42a01361fb56400c9e0dd9e76e13d500e9f035a
lakshay17 <lakshayjain01lj@gmail.com>
2024-09-27 20:39:16 +0530

    RewardValue class added

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,7 +1,6 @@
 public class RewardValue {
     private final double cashValue;
     private final double milesValue;
-
     private static final double MILES_TO_CASH_RATE = 0.0035;
 
     public RewardValue(double cashValue){
